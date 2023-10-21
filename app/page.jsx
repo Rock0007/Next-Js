1112a1ac6bf7ed22a1fa5935ab9dd387a298abb5
Kapil As <askapil07@gmail.com>
2023-10-22 00:59:57 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold mb-5">Welcome</h1>
-      <p className="mb-5">This is a demo site for the BluEmbers</p>
+      <p className="mb-5">This is a demo site for the Clerk Authentication</p>
     </>
   );
 }
